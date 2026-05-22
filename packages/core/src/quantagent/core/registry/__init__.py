8cf998aa2176cc4e897bfb0e32a8e9569769c7fe
xxs588 <2127069700@qq.com>
2026-05-22 18:54:53 +0800

    feat(core): 提供插件注册表查询 facade

--- packages/core/src/quantagent/core/registry/__init__.py
+++ packages/core/src/quantagent/core/registry/__init__.py
@@ -7,13 +7,19 @@ from quantagent.core.registry.models import (
     PluginStatus,
     PluginType,
 )
+from quantagent.core.registry.scanner import RegistryScanner
+from quantagent.core.registry.service import PluginRegistry, build_plugin_registry, summarize_plugin_records
 
 __all__ = [
     "PluginError",
     "PluginManifest",
     "PluginRecord",
+    "PluginRegistry",
     "PluginScanSummary",
     "PluginSource",
     "PluginStatus",
     "PluginType",
+    "RegistryScanner",
+    "build_plugin_registry",
+    "summarize_plugin_records",
 ]
